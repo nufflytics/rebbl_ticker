9fd8847b213d07fa017552007039e06f0590b591
Stephen Pearce <spearce@me.com>
2018-03-12 22:25:00 +1100

    removing test stuff

--- ui.R
+++ ui.R
@@ -4,7 +4,5 @@ shiny::fluidPage(
   shiny::includeCSS("www/ticker.css"),
   fluidRow(column(3,checkboxGroupInput("leagues", "League", c("REL" = "REBBL - REL", "Gman" = "REBBL - Gman", "Big O" = "REBBL - Big O"), selected = c("REL" = "REBBL - REL", "Gman" = "REBBL - Gman", "Big O" = "REBBL - Big O"))),
   column(3,dateInput("since", "Since", value = lubridate::today()))),
-  uiOutput("score_ticker"),
-  verbatimTextOutput("test"),
-  shiny::includeScript("www/ticker.js")
+  uiOutput("score_ticker")
 )
